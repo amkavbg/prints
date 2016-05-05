f83d0994a7e4656b4fa2407e71a646ede2b32685
amkavbg <amka.vbg@gmail.com>
2016-05-05 12:09:38 +0300

    1

--- src/ru/tokido/Main.java
+++ src/ru/tokido/Main.java
@@ -11,38 +11,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ArrayList<Printer> printerArrayList = new ArrayList<Printer>();
         SnmpQuerier snmpquerier = new SnmpQuerier();
+        //OrganizePrint organizePrint = new OrganizePrint();
 
         //читаем конфиги
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         Scanner in = new Scanner(new File("ip.txt"));
         while (in.hasNextLine()) list.add(in.nextLine());
         //создаем объекты "принтер" с заполнеными пол€ми IP
         for (String p : list) {
             Printer printer = new Printer();
             printer.setIp(p);
-            printer.sayHello();
-            System.out.println(printer);
             //обращаемс€ к устройству и вы€сн€ем его модель из OID
             try {
                 try {
                     snmpquerier.start();
                     printer.setModel(snmpquerier.send(p, "1.3.6.1.2.1.25.3.2.1.3.1"));
+                    //System.out.println(printer);
                     System.out.println(printer.getModel());
+
+
                 } finally {
                     snmpquerier.stop();
                 }
             } catch (IOException e) {
-                System.out.println(e.toString());
+                    System.out.println(e.toString());
             }
         }
 
-
-
-
-
-        //заполн€ем атрибуты класса и получаем объект с OID но без данных
-
-        //
     }
 
 }
--- src/ru/tokido/OrganizePrint.java
+++ src/ru/tokido/OrganizePrint.java
@@ -3,9 +3,12 @@ package ru.tokido;
 /**
  * Created by oparin_ag on 20.04.2016.
  */
-public class OrganizePrint {
-    //Формирование объекта принтер, от пустого класса
-    //до заполненного с полями, но без данных
+public class OrganizePrint extends SnmpQuerier {
+    private Printer printer = null;
+    //получает пустой класс
+
+    //Формирование объекта принтер, от пустого класса до заполненого данными
+
 
 
     //возврат готового объекта
